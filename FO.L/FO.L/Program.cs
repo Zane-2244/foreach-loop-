835ede88e9383c1fd61303a296f943d671113714
Zane-2244 <49199705+Zane-2244@users.noreply.github.com>
2019-04-15 15:09:32 +0200

    Add files via upload

---
+++ FO.L/FO.L/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FO.L
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[] { 1, 2, 3, 4 };
+            foreach(var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
